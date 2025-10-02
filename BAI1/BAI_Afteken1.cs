7d1512390fd79989eeb27bb9cd4ddb231b439741
joshitachu <118347322+joshitachu@users.noreply.github.com>
2025-10-02 14:00:55 +0200

    feedback

--- BAI1/BAI_Afteken1.cs
+++ BAI1/BAI_Afteken1.cs
@@ -14,39 +14,45 @@ namespace BAI
         // ***************
         public static UInt64 Opg1aDecodeBase27(string base27getal)
         {
+            /* Zuja: voeg nog wat nuttige commentaar toe, 
+            bijv. variabele uitleg, wat de loop doet, etc. */            
+
             UInt64 result = 0;
-                foreach (char c in base27getal)
-                {
-                    int digit = BASE27CIJFERS.IndexOf(c);
-                if (digit < 0)
-                    Console.WriteLine(" not found buddy");
+            foreach (char c in base27getal) //loopt door elke karakter in de string
+            {
 
-                    if (result > (UInt64.MaxValue - (UInt64)digit) / 27UL)
-                        return UInt64.MaxValue;
+                int digit = BASE27CIJFERS.IndexOf(c); //controleert of het karakter in de base27 cijfers zit
+                if (digit < 0)// als het karakter er niet in zit returned het -1 , dit checken we hier
+                    // Zuja: in dit soort methodes mag je geen Console.WriteLine doen.
+                    // Je zou hier een exceptie moeten gooien.
+                    throw new Exception("Ongeldig teken gevonden in base27-getal.");// exceptie gooien
 
-                    result = result * 27 + (UInt64)digit;
-                }
-                return result;
+                // Zuja: Deze Check for overflow is goed bedoeld, maar niet correct.
+                // Zie ook jouw result aan het einde van deze method.
+                if (result > (UInt64.MaxValue - (UInt64)digit) / 27UL) // Check voor overflow:
+                    throw new Exception("base getal is te groot."); // exceptie gooien
+
+                result = result * 27 + (UInt64)digit;// resultaat berekenen 
+            }
+            return result;
         }
         public static string Opg1bEncodeBase27(UInt64 base10getal)
         {
-            String result="";
 
-            if (base10getal == 0){
+            /* Zuja: voeg nog wat nuttige commentaar toe, 
+            bijv. variabele uitleg, wat de loop doet, etc. */            
+            String result="";
+            if (base10getal == 0){// speciaal geval als het getal 0 is return dan "-"
                 return "-";
             }
-            var chars = new List<char>();
-            UInt64 n = base10getal;
-
-            while (n > 0)
+            while (base10getal > 0)// zolang het getal groter is dan 0
             {
-                int digit = (int)(n % 27);
-                chars.Add(BASE27CIJFERS[digit]);
-                n /= 27;
-                
+                int digit = (int)(base10getal % 27); // bepaal de rest bij deling door modulus 27
+                result= BASE27CIJFERS[digit] + result;// voeg het karakter toe aan het result
+                // Zuja: onderstaande regel is correct, maar je kunt ook anders oplossen, zonder List.
+                base10getal /= 27;// deel n door 27
             }
-            chars.Reverse();
-            result = new string(chars.ToArray());
+            // Zuja: Twee overbodige regels.
             return result;
 
         }
@@ -58,10 +64,10 @@ namespace BAI
         {
             var stack = new Stack<UInt64>();
 
-            foreach (var woord in woorden)
+            foreach (var woord in woorden)// loop door elk woord in de lijst
             {
                 UInt64 number = Opg1aDecodeBase27(woord);
-                stack.Push(number);
+                stack.Push(number);// zet het getal op de stack
             }
             return stack;
         }
@@ -69,15 +75,17 @@ namespace BAI
         {
             
             var queue = new Queue<string>();
-            ulong max = 27 * 27 * 27 - 1;
+            // Zuja: Waarom geen Const? En waarom -1?
+            const ulong max = 27 * 27 * 27;// geeft het max waarde van een 3 letter woord in base27
 
-            while (woordstack.Count > 0)
+            while (woordstack.Count > 0)// zolang er woorden in de stack zitten
             {
                 ulong woord = woordstack.Pop();
+                // Zuja: Verband met die -1?
                 if (woord <= max)
                 {
                     string decoded = Opg1bEncodeBase27(woord);
-                    queue.Enqueue(decoded);
+                    queue.Enqueue(decoded);// zet het woord in de queue
                 }
             }
 
